fbc8b0d51760c667ea280bc8cec81503a26b406f
serii833 <Sergei.Babski@gmail.com>
2025-08-12 19:38:51 +0300

    change source compatibility to 21

--- build.gradle.kts
+++ build.gradle.kts
@@ -71,8 +71,8 @@ tasks {
     }
 
     withType<JavaCompile> {
-        sourceCompatibility = "17"
-        targetCompatibility = "17"
+        sourceCompatibility = "21"
+        targetCompatibility = "21"
     }
 
     patchPluginXml {
